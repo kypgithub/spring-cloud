38b381b7ef2dc0caaa263a1d8c4d3ea600dace5d
kypgithub <646918332@qq.com>
2019-11-28 23:08:04 +0800

    0.1

---
+++ spring-cloud-service1/src/main/java/com/abugong/cloud/SpringCloudService1Application.java
@@ -0,0 +1,27 @@
+package com.abugong.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudService1Application.class, args);
+	}
+	//因为新版本的security默认开启了csrf拦截, 所以需要禁用csrf功能
+	@EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
+
+}
