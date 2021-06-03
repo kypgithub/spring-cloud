2660ebd84ef9ba5b163ccb7ce8f8462741728657
kypgithub <646918332@qq.com>
2021-06-03 17:12:05 +0800

    急急急

---
+++ spring-cloud-common/src/main/java/com/abugong/cloud/common/CommonApplication.java
@@ -0,0 +1,16 @@
+package com.abugong.cloud.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: kyp
+ * @Date: 2021/6/3 17:01
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class);
+    }
+}
